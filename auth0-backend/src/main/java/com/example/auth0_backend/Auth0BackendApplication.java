f15271eff4b8a3dd09902d6c3705dd34d2cf13ec
johram123 <johramq@gmail.com>
2025-08-09 23:48:02 -0600

    First push

---
+++ auth0-backend/src/main/java/com/example/auth0_backend/Auth0BackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.auth0_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Auth0BackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Auth0BackendApplication.class, args);
+	}
+
+}
---
+++ auth0-backend/src/test/java/com/example/auth0_backend/Auth0BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.auth0_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Auth0BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
